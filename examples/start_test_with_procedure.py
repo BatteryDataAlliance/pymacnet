b02c35ae5c26d845cc2c88f336abd8de44c1cd25
Chintan Pathak <chintan.pathak@gmail.com>
2022-09-26 14:57:06 -0700

    confirmed that I can read stats and start test using MacNet

--- examples/start_test_with_procedure.py
+++ examples/start_test_with_procedure.py
@@ -1,19 +1,23 @@
 '''
-Example how to create a connection with the Maccor interface and read channel status
+Example how to start a test with a test procedure using the Maccor interface.
 '''
 
+import pymacnet
 import json
-import pymacnet 
 import time
 
 config_path = 'example_config_1.json'
 with open(config_path, 'r') as file:
     config_dict = json.load(file)
-    
+
 maccor_interface = pymacnet.MaccorInterface(config_dict)
 if not maccor_interface.create_connection():
     print("failed to create connection!")
 
-for i in range(0,5):
+print(maccor_interface.read_status())
+result = maccor_interface.start_test_with_procedure()
+print(result)
+
+for i in range(0,6):
     print(maccor_interface.read_status())
     time.sleep(0.5)
