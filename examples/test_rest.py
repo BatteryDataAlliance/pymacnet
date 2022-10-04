f7ec6eb00ac08a6a3d1e88892be55757b09cff41
Chintan Pathak <chintan.pathak@gmail.com>
2022-10-04 11:48:22 -0700

    Can now set rest

---
+++ examples/test_rest.py
@@ -0,0 +1,29 @@
+'''
+Example of how to start a test with direct control and control charge/discharge current.
+'''
+import pymacnet
+import json
+import time
+import sys
+
+config_path = 'example_config_1.json'
+with open(config_path, 'r') as file:
+    config_dict = json.load(file)
+
+maccor_interface = pymacnet.MaccorInterface(config_dict)
+if not maccor_interface.create_connection():
+    sys.exit("failed to create connection!")
+
+if maccor_interface.start_test_with_direct_control():
+    print("Test started!")
+else:
+    sys.exit("Failed to start test!")
+
+time.sleep(3) # Must wait at least 100 ms between trying to set control
+
+
+response = maccor_interface.set_direct_mode_output(current_a = 0)
+
+print(response)
+
+print("Test Complete!")
