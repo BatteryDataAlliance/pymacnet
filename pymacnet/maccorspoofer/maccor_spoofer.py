ddc556f33d1ddd048966718c293707df776aa580
Thomas Nevitt <zander@Thomass-MacBook-Pro-5.local>
2022-12-19 10:59:10 -0800

    Changed maccor spoofer a bit

--- pymacnet/maccorspoofer/maccor_spoofer.py
+++ pymacnet/maccorspoofer/maccor_spoofer.py
@@ -8,8 +8,7 @@ class MaccorSpoofer:
     Class to mimic behavior of Maccor cycler MacNet control server. 
     """
     
-    __receive_msg_timeout_s = 1
-    __msg_buffer_size_bytes = 1024
+    __client_connect_timeout_s = 1
     __stop_servers_lock = threading.Lock()
     __stop_servers = False
 
@@ -23,8 +22,16 @@ class MaccorSpoofer:
             A configuration dictionary containing the server ip address and ports to use.
         """
         self.config = config
-        self.__json_server_thread = threading.Thread( target=self.__json_server_loop, args=(), daemon=True)
-        self.__tcp_server_thread = threading.Thread( target=self.__tcp_server_loop, args=(), daemon=True)
+
+        json_server_config = {'ip':config['server_ip'],'port':config['json_port']}
+        self.__json_server_thread = threading.Thread( target=self.__server_loop, 
+                                                        args=( json_server_config, JsonWorker,), 
+                                                        daemon=True)
+
+        tcp_server_config = {'ip':config['server_ip'],'port':config['tcp_port']}
+        self.__tcp_server_thread = threading.Thread( target=self.__server_loop,
+                                                         args=( tcp_server_config, TcpWorker,), 
+                                                        daemon=True)
 
     def start(self):
         """
@@ -37,7 +44,7 @@ class MaccorSpoofer:
 
     def __server_loop( self, sock_config : dict, Worker):  
         """
-        Creates a forever loop to service client socket requests.
+        Creates a server and forever loop to service client socket requests.
         ----------
         sock_cofig : dict
             A configuration for the socket containing the IP and port number.
@@ -49,8 +56,8 @@ class MaccorSpoofer:
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind((sock_config["server_ip"], sock_config["json_port"]))
-        sock.settimeout(self.__receive_msg_timeout_s)
+        sock.bind((sock_config["ip"], sock_config["port"]))
+        sock.settimeout(self.__client_connect_timeout_s)
         sock.listen()
         
         while True:
@@ -68,62 +75,6 @@ class MaccorSpoofer:
                 # Remove any workers that made have died from disconnecting clients.
                 client_workers[:] = [worker for worker in client_workers if worker.is_alive()]                         
 
-    def __json_server_loop(self):
-        """
-        Starts a the JSON server in a forever loop. Breaks when stop method is called. Can handle multiple client connecitons.
-        """
-
-        # List that will hold all the workers to service client connections.
-        client_workers = [] 
-
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind((self.config["server_ip"], self.config["json_port"]))
-        sock.settimeout(self.__receive_msg_timeout_s)
-        sock.listen()
-        
-        while True:
-            try: 
-                client_connection, client_address = sock.accept()
-                client_workers.append(JsonWorker(client_connection))
-            except socket.timeout:
-                with self.__stop_servers_lock:
-                    # If stop commmand is issued then kill all workers.
-                    if self.__stop_servers:
-                        for worker in client_workers:
-                            if worker.is_alive():
-                                worker.kill_worker()
-                        break
-                # Remove any workers that made have died from disconnecting clients.
-                client_workers[:] = [worker for worker in client_workers if worker.is_alive()]
-
-    def __tcp_server_loop(self):
-        """
-        Starts a the JSON server in a forever loop. Breaks when stop method is called. Can handle multiple client connecitons.
-        """
-
-        # List that will hold all the workers to service client connections.
-        client_workers = [] 
-
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind((self.config["server_ip"], self.config["tcp_port"]))
-        sock.settimeout(self.__receive_msg_timeout_s)
-        sock.listen()
-        
-        while True:
-            try: 
-                client_connection, client_address = sock.accept()
-                client_workers.append(TcpWorker(client_connection))
-            except socket.timeout:
-                with self.__stop_servers_lock:
-                    if self.__stop_servers:
-                        for worker in client_workers:
-                            if worker.is_alive():
-                                worker.kill_worker()
-                        break
-                client_workers[:] = [worker for worker in client_workers if worker.is_alive()]
-
     def stop(self):
         """
         Stop the server loops
@@ -139,8 +90,8 @@ class MaccorSpoofer:
 class SocketWorker:
     '''
     Generic worker class that will respond to client socket requests. 
-    Default setup as an echo client. Child classes can overwrite the 
-    the `__process_client_msg` method with their own responses
+    Default setup as an echo server. Child classes should overwrite the 
+    the `_process_client_msg()` method with their own responses.
     '''
     __receive_msg_timeout_s = 1
     __msg_buffer_size_bytes = 1024
@@ -149,7 +100,7 @@ class SocketWorker:
 
     def __init__(self, s: socket.socket):
         """
-        Creates the thread to service JSON client requests
+        Creates the thread to service client requests.
         ----------
         s : socket.socket
             Socket connection to client.
@@ -160,7 +111,9 @@ class SocketWorker:
     
     def ___service_loop(self, s: socket.socket):
         """
-        Forever loop to service JSON client requests.
+        Forever loop to service client requests. Wait to receive a message. If no messages is 
+        received before the timeout then check to see if stop command has been issued. Loop is 
+        also broken if client breaks conenction by sending b''. 
         ----------
         s : socket.socket
             Socket connection to client.
@@ -272,7 +225,7 @@ class JsonWorker(SocketWorker):
 
 class TcpWorker(SocketWorker):
     '''
-    Class to handle requests from TCP JSON socket clients.
+    Class to handle requests from TCP socket clients.
     Currently just implemented as an echo server.
     '''
     pass
